64f979d3ef6353c00b416b06dd25a914825a4c49
Maxime Miguet <maxime.miguet@edu.univ-fcomte.fr>
2025-12-08 14:58:56 +0100

    add shopLogin

---
+++ src/views/ShopBuy.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="shop-buy">
+    <div class="left-panel">
+      <ItemsList />
+    </div>
+    <div class="right-panel">
+      <BasketList />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CheckedList from '@/components/CheckedList.vue'
+</script>
+
+<style scoped>
+.shop-buy {
+  display: flex;
+  gap: 2rem;
+}
+.left-panel {
+  flex: 1;
+}
+.right-panel {
+  flex: 1;
+}
+</style>
